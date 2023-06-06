a9ab939e71e439d6182922be2d4a0b5b8b80ca02
Demetic <iclekdemet@gmail.com>
2023-06-05 22:16:29 -0400

    version 1

---
+++ src/main/java/Execute01.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Execute01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1. step : Register Driver - (optional)
+        Class.forName("org.postgresql.Driver");
+
+        //2.step : Create Connection to get connected to DB
+       Connection con= DriverManager.getConnection("jdbc:postgresql://localhost:5432/jdbc_db",
+               "dev_user","Password");
+
+       //3. step : Create Statement-- to execute SQL queries
+        Statement statement =con.createStatement();
+
+        //to test if we have created connection to DB
+        //System.out.println("Connected Successfully");
+
+        //4. step : Execute SQL queries
+        //TASK: create a table named "employee" with column names of :
+        // "employee_id", "employee_name", "salary"
+//        boolean sql1 = statement.execute("CREATE TABLE employee (employee_id INT, employee_name VARCHAR(50), salary REAL)");
+//        System.out.println("sql1: " + sql1);
+        /*
+        --execute() returns boolean value and can be used for DDL (Data Definition Language) or
+            DQL(Data Query Language)
+            --If it is used with DDL it return false
+            --If it is used DQL (Select...)it return true
+
+         */
+        //TASK 2: add Varchar (20) column name "city" to employee table
+        String query ="ALTER TABLE employee ADD COLUMN city VARCHAR(20)";
+
+//        boolean sql2=statement.execute(query);
+//        System.out.println("sql2 = " + sql2);
+        //TASK 3: Delete employee table from SCHEMA
+        String query1="DROP TABLE employee";
+        statement.execute(query1);
+
+        //5. step: close connection and statement
+        statement.close();
+        con.close();
+
+
+    }
+}
---
+++ src/main/java/ExecuteQuery01.java
@@ -0,0 +1,69 @@
+import java.sql.*;
+
+public class ExecuteQuery01 {
+    public static void main(String[] args) throws SQLException {
+        //Create connection
+        Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/jdbc_db","dev_user","Password");
+
+        //Create statement
+        Statement statement=con.createStatement();
+        System.out.println("********************** Task 1 **************************************");
+        //TASK-1. GET/SELECT  "country_name" from countries table with ID between 5 and 10
+        String query1="SELECT country_name FROM countries WHERE id BETWEEN 5 AND 10";
+        boolean sql1=statement.execute(query1);
+        System.out.println("sql1 = " + sql1);
+
+        ResultSet resultSet=statement.executeQuery(query1);
+        //resultSet.getString(1);
+        //We can use column index number
+        //System.out.println(resultSet.next());
+//        resultSet.next();
+//        System.out.println(resultSet.getString("country_name"));
+
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("country_name"));// get column by columnLabel
+            //System.out.println(resultSet.getString(1));  //get column by index number.
+
+        }
+        //ResultSet has method such first(), last(), or next()
+        //There is no method to iterate backward
+        System.out.println("*********************** Task 2 *************************88");
+
+        //TASK - 2: Get "phone_code" and "country_name" from countries table,
+        // whose phone code is greater than 200
+        String query2="SELECT phone_code,country_name FROM countries WHERE phone_code>500";
+        ResultSet resultSet2=statement.executeQuery(query2);
+
+        while(resultSet2.next()){
+            System.out.println(resultSet2.getInt("phone_code")+"--"+
+                    resultSet2.getString("country_name"));
+        }
+        System.out.println("************************ Task 3 *****************");
+        //TASK-3. Get all information about the developers whose salary is lowest
+        String query3 = "SELECT * FROM developers WHERE salary = (SELECT min(salary) FROM developers)";
+        ResultSet resultSet3 = statement.executeQuery(query3);
+
+        while (resultSet3.next()){
+            System.out.println(resultSet3.getInt("id")+" -- "+resultSet3.getString("name")+
+                    " -- "+ resultSet3.getString("prog_lang") +" -- "+ resultSet3.getDouble("salary"));
+        }
+        System.out.println("******************************* Task 4 **********************");
+
+        //TASK - 4 : Display students' name and grade whose grades are higher than  average passing grade of departments.
+        String query4 = "SELECT name, grade FROM students WHERE grade> (SELECT AVG(pass_grade) FROM departments)";
+        ResultSet resultSet4=statement.executeQuery(query4);
+        while(resultSet4.next()){
+            System.out.println(resultSet4.getString("name")+" -- "+resultSet4.getInt("grade"));
+        }
+
+
+
+        }
+
+
+
+
+
+
+    }
+
